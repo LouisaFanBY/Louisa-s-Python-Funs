b2e3259850476918f0b7b241f4dae9b5bed8abb6
Louisa FAN <pourlfy1@gmail.com>
2022-05-29 11:12:29 +0800

    click and smile, face

---
+++ ClickAndSmile_ls.py
@@ -0,0 +1,20 @@
+# ClickAndSmile.py by Louisa
+import random
+import turtle
+t = turtle.Pen()
+t.speed(0)
+t.hideturtle()
+turtle.bgcolor("red")
+def draw_smiley(x,y):
+    print("x: ", x, " y: ", y)
+    t.penup()
+    t.setpos(x,y)
+    t.pendown()
+    # Face
+    t.pencolor("yellow")
+    t.fillcolor("pink")
+    t.begin_fill()
+    t.circle(50)
+    t.end_fill()
+
+turtle.onscreenclick(draw_smiley)
