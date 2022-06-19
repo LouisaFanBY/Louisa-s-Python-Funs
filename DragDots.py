99e3dacac64063dfe6b244b2a4fcb521c02a347e
Louisa FAN <pourlfy1@gmail.com>
2022-06-19 14:13:42 +0800

    drag

--- DragDots.py
+++ DragDots.py
@@ -1,18 +1,23 @@
-# ClickDots.py
+# ClickDots.py by Louisa
 import pygame
 pygame.init()
 screen = pygame.display.set_mode([1200,1000])
-pygame.display.set_caption("Click to draw")
+pygame.display.set_caption("Click and drag to draw")
 keep_going = True
 RED = (255,0,0)
 radius = 15
+mousedown = False
 while keep_going:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             keep_going = False
         if event.type == pygame.MOUSEBUTTONDOWN:
-            spot = event.pos
-            pygame.draw.circle(screen, RED, spot, radius)
+            mousedown = True
+        if event.type == pygame.MOUSEBUTTONUP:
+            mousedown = False
+    if mousedown:
+        spot = pygame.mouse.get_pos()
+        pygame.draw.circle(screen, RED, spot, radius)
     pygame.display.update()
 
 pygame.quit()
