367543c48c8a9f45546d4879ea4fa7f1de8500e7
Louisa FAN <pourlfy1@gmail.com>
2022-11-18 22:07:15 +0800

    Line 50

--- SmileyPop2.py
+++ SmileyPop2.py
@@ -27,5 +27,24 @@ class Smiley(pygame.sprite.Sprite):
         pygame.sprite.Sprite.__init__(self)
         self.image = pic
         self.scale = random.randrange(10,100)
-        self.image = pygame.transform.scale(self.image,
-        (self.scale,self.scale))
+        self.image = pygame.transform.scale(self.image,(self.scale,self.scale))
+        self.rect = self.image.get_rect()
+        self.pos = pos
+        self.rect.x = pos[0] - self.scale/2
+        self.rect.y = pos[1] - self.scale/2
+        self.xvel = xvel
+        self.yvel = yvel
+    def update(self):
+        self.rect.x += self.xvel
+        self.rect.y += self.yvel
+        if self.rect.x <= 0 or self.rect.x > screen.get_width() - self.scale:
+            self.xvel = -self.xvel
+        if self.rect.y <= 0 or self.rect.y > screen.get_height() - self.scale:
+            self.yvel = -self.yvel
+while keep_going:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            keep_going = False
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if pygame.mouse.get_pressed() [0]:  # Left mouse button, draw
+                mousedown = True
