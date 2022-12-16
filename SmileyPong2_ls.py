03035eda464fc1d6c3b0089efd773a78f105157f
Louisa FAN <pourlfy1@gmail.com>
2022-12-16 21:46:28 +0800

    Smiley

--- SmileyPong2_ls.py
+++ SmileyPong2_ls.py
@@ -18,3 +18,33 @@ paddlew = 200
 paddleh = 25
 paddlex = 300
 paddley = 550
+picw = 100
+pich = 100
+points = 0
+lives = 5
+font = pygame.font.SysFont("Times", 24)
+while keepGoing:   # Game loop
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            keepGoing = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_F1:   # F1 = New Game
+                points = 0
+                lives = 100000000
+                picx = 0
+                picy = 0
+                speedx = 5
+                speedy = 5
+
+    picx += speedx
+    picy += speedy
+
+    if picx <= 0 or picx >= 700:
+        speedx = -speedx * 1.1
+    if picy <= 0:
+        speedy = -speedy +1
+    if picy >= 500:
+        lives -= 1
+        speedy = -5
+        speedx = 5
+        picy = 499
